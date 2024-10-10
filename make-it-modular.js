73929c3d5d963ea903fcbb56a8c8053bd37aebdb
RafiRafi246 <golamhayderrafi@gmail.com>
2024-10-11 02:17:03 +0600

    lyn

---
+++ make-it-modular.js
@@ -0,0 +1,8 @@
+const mymodule = require('./mymodule');
+
+mymodule(process.argv[2], process.argv[3], (err, data) => {
+    err ? console.log('Error Reading Directory: ', err) :
+        data.forEach(file => {
+            console.log(file);
+        });
+});
